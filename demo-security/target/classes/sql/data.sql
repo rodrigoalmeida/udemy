4ad9d5fc2f9b89e6061541b56a8d19ffa8eedf0c
Rodrigo A. R. de Almeida <rodrigoalmeida@ymail.com>
2021-01-30 00:47:33 -0300

    Delete demo-security/target directory

--- demo-security/target/classes/sql/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO perfis VALUES (1,'ADMIN'),(2,'MEDICO'),(3,'PACIENTE');
-
-INSERT INTO perfis VALUES (1,'ADMIN')
- Select '1448523' Where not exists(select * from tablename where code='1448523')
